4772ec44a4a9065dc1d772085b0a3abd147291bc
Apshana <90459816+APSHANASHAIK@users.noreply.github.com>
2023-02-19 20:00:56 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,25 @@
+
+#//sorting
+numbers=[1,3,5,6,7]
+numbers.sort()
+print (numbers)
+#//reverse
+numbers=[1,3,5,6,7]
+numbers.reverse
+print(numbers)
+#append
+numbers=[1,3,5,6,7]
+numbers.append(0)
+print(numbers)
+numbers=[1,3,5,6,7]
+numbers.pop(4)
+print(numbers)
+chatiteams=['pizza','burger','french fries','ice cream']
+print(chatiteams[2])
+
+
+
+a=1
+b=2
+a,b=b,a 
+print(a,b)
